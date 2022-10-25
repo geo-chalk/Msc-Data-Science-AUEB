6e3f25472551190788dc270a3f6bdd54228c9126
geo-chalk <geo.chalkiopoulos@gmail.com>
2022-10-25 22:15:10 +0300

    update: add GLOBALS

---
+++ SNA/Project_2/utils/GOBALS.py
@@ -0,0 +1,4 @@
+BASIC_COLS: list = ["Nodes", "NodeOutDeg", "RewireProb"]
+DEGREE_COLS: list = ["highest_deg_ID", "highest_deg_degree"]
+HUB_COLS: list = ["highest_hubscore_ID", "highest_hubscore_degree"]
+AUTH_COLS: list = ["highest_authscore_ID", "highest_authscore_degree"]
