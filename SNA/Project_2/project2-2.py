ccf2a63e3210e3bb3ac2c620ee67204a04465ca3
jojoshulk <jojoshulk@gmail.com>
2022-10-31 23:59:43 +0200

    create a csv for each algorithm, generate the pagerank plots

--- SNA/Project_2/project2-2.py
+++ SNA/Project_2/project2-2.py
@@ -1,22 +1,25 @@
 import snap
 import pandas as pd
-from utils.helper_classes import AlgorithmComparison
+from utils.helper_classes import AlgorithmComparison, CompareMetrics, ResultPlot
 from pprint import pprint
 import random
 from utils.GOBALS import *
 
 comm_values: list = [x for v in COMMUNITLY_COLS.values() for x in v]
 
+
 def generate_dataframe() -> pd.DataFrame:
     """Generates an empty DataFrame with the required columns"""
 
+
     df_cols: list = BASIC_COLS + DEGREE_COLS + HUB_COLS + AUTH_COLS + comm_values
-    return pd.DataFrame(columns=df_cols)
+    df: pd.DataFrame = pd.DataFrame(columns=df_cols)
+    return df
 
 def print_dataframe(df: pd.DataFrame) -> None:
     """Prints there dataframe in a readable format"""
     print("-" * 130)
-    print(df[["Nodes"] + BASIC_COLS + DEGREE_COLS].to_string(index=False))
+    print(df[BASIC_COLS + DEGREE_COLS].to_string(index=False))
     print("-" * 130)
     print(df[["Nodes"] + HUB_COLS + AUTH_COLS].to_string(index=False))
     print("-" * 130)
@@ -25,21 +28,52 @@ def print_dataframe(df: pd.DataFrame) -> None:
 
 
 def main():
+
+    # --- read df and max index --- #
     df = generate_dataframe()
 
+    max_nodes: int = 0
     threshold_reached: bool = False
-    # --- For various nodes, compare and store algorithms --- #
-    for i, nodes in enumerate(range(50, 500, 20)):
+    for algorithm in ["CommunityCNM", "CommunityGirvanNewman"]:
 
-        df.loc[i, BASIC_COLS] = [nodes, random.randint(5, 20), random.randint(0, 100)/100]
-        threshold_reached = AlgorithmComparison(nodes=nodes, iter=i, df=df).print_results()
-        if threshold_reached:
-            break
-        print_dataframe(df)
+        # --- For various nodes, compare and store algorithms --- #
+        for i, nodes in enumerate(range(50, 1000000, 1000)):
+
+            print(f"Running for {nodes} nodes")
+
+            # --- if run exists skip it --- #
+            if not df.loc[df.Nodes == nodes].empty:
+                pass
+            else:
+                df.loc[i, BASIC_COLS] = [nodes, random.randint(5, 20), random.randint(0, 100) / 100]
+
+            # --- run the community detection algorithm --- #
+            threshold_reached = AlgorithmComparison(nodes=nodes, iter=i, df=df).print_results(_algorithms=[algorithm])
+            if threshold_reached:
+                break
+
+            # --- save max nodes reached --- #
+            if nodes > max_nodes:
+                max_nodes = nodes
+            print_dataframe(df)
+
+
+            # --- Save results to csv --- #
+            df.to_csv(f"project2-2_{algorithm}.csv", index=False)
 
     print(df.to_string(index=False))
-    # --- Save results to csv --- #
-    df.to_csv("project2-2.csv")
+
+    # --- find max calculated nodes and compute metrics --- #
+    nodes = max_nodes
+    print("\n"*5, f"Generating plots for {nodes} nodes")
+    df_pagerank: pd.DataFrame = CompareMetrics(nodes=nodes).get_metrics()
+
+    # sort by pagerank and print
+    df_pagerank = df_pagerank.sort_values("PageRank", ascending=False).head(30)
+    print(df_pagerank)
+
+    ResultPlot(df_pagerank).generate_plots()
+
 
 if __name__ == '__main__':
     main()
--- SNA/Project_2/utils/helper_classes.py
+++ SNA/Project_2/utils/helper_classes.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import snap
 import pandas as pd
+import matplotlib.pyplot as plt
 from .GOBALS import *
 import time
 import random
@@ -75,10 +76,9 @@ class AlgorithmComparison:
         """Function that generates a random graph using the Watts-Strogatz model."""
 
         Rnd = snap.TRnd(1, 0)
-        return snap.GenSmallWorld(self.nodes, self.df.at[self.iter, "NodeOutDeg"],
+        return snap.GenSmallWorld(self.nodes, int(self.df.at[self.iter, "NodeOutDeg"]),
                                   self.df.at[self.iter, "RewireProb"],
                                   Rnd)
-
 
     def highest_degrees(self) -> None:
         """Print the node with the highest degree along with it's degree"""
@@ -87,23 +87,20 @@ class AlgorithmComparison:
             if NI.GetDeg() > highest_deg[0]:
                 highest_deg = [NI.GetDeg(), NI.GetId()]
 
-        # print(f"The Node with the highest degree is:")
-        # print(f"ID: {highest_deg[1]: >6} \nDegree: {highest_deg[0]: <10}")
+
         self.df.loc[self.iter, DEGREE_COLS] = [highest_deg[1], highest_deg[0]]
 
     def hub_and_auth(self) -> None:
         """Print the is and scores of the nodes with the highest Hubs and Authorities scores."""
         NIdHubH, NIdAuthH = self.graph.GetHits()
 
-        # --- Buhs Score --- #
+        # --- Hubs Score --- #
         highest_hub: list = [0, -1]
         for item in NIdHubH:
             if NIdHubH[item] > highest_hub[0]:
                 highest_hub = [NIdHubH[item], item]
         # update df and print
         self.df.loc[self.iter, HUB_COLS] = [highest_hub[1], highest_hub[0]]
-        # print(f"The Node with the highest hub score is:")
-        # print(f"ID: {highest_hub[1]: >6} \nScore: {highest_hub[0]: >7.4f}")
 
         # --- Authorities Score --- #
         highest_auth: list = [0, -1]
@@ -112,8 +109,7 @@ class AlgorithmComparison:
                 highest_auth = [NIdAuthH[item], item]
         # update df and print
         self.df.loc[self.iter, AUTH_COLS] = [highest_auth[1], highest_auth[0]]
-        # print(f"The Node with the highest auth score is:")
-        # print(f"ID: {highest_auth[1]: >6} \nScore: {highest_auth[0]: >7.4f}")
+
 
     @exit_after(60*10)
     def run_community_detection(self, _alg):
@@ -125,10 +121,10 @@ class AlgorithmComparison:
         self.df.loc[self.iter, COMMUNITLY_COLS.get(_alg)] = [end-start, modularity]
         print(f"The modularity of the network is {modularity:.3f}.\nExecution time: {end-start:.3f}s\n")
 
-    def print_results(self):
+    def print_results(self, _algorithms: list = ["CommunityCNM", "CommunityGirvanNewman"]):
         self.highest_degrees()
         self.hub_and_auth()
-        for alg in ("CommunityCNM", "CommunityGirvanNewman"):
+        for alg in _algorithms:
             try:
                 self.run_community_detection(alg)
             except KeyboardInterrupt:
@@ -140,4 +136,155 @@ class AlgorithmComparison:
         return False
 
 
+class CompareMetrics:
+    def __init__(self, nodes: int):
+        self.nodes = nodes
+        self.graph = self.generate_graph()
 
+    def generate_graph(self) -> snap.GenSmallWorld:
+        """Function that generates a random graph using the Watts-Strogatz model."""
+
+        Rnd = snap.TRnd(1, 0)
+        return snap.GenSmallWorld(self.nodes, random.randint(5, 20),
+                                  random.randint(0, 100) / 100,
+                                  Rnd)
+
+    @staticmethod
+    def snap_to_array(TIntFltH: snap.TIntFltH, metric: str) -> dict:
+        """
+        Returns a dictionary having the metric (eg PageRank) as key and value the calulcated score for each node.
+
+        Args:
+            TIntFltH: snap.TIntFltH object
+            metric: str describing the calculated score
+
+        Returns:
+            dict of the format {str: list}
+        """
+        _val: list = [TIntFltH[item] for item in TIntFltH]
+
+        return {metric: _val}
+
+    def get_PageRank(self) -> dict:
+        """Calculates and returns the PageRank for all nodes"""
+        PRankH = self.graph.GetPageRank()
+
+        return self.snap_to_array(PRankH, "PageRank")
+
+    def get_betweeness_centr(self)-> dict:
+        """Calculates and returns the Betweenness Centrality for all nodes"""
+        Nodes, Edges = self.graph.GetBetweennessCentr(1.0)
+        return self.snap_to_array(Nodes, "BetweennessCentr")
+
+    def get_closeness_centr(self) -> dict:
+        """Calculates and returns the closeness Centrality for all nodes"""
+        CloseCentr = [self.graph.GetClosenessCentr(NI.GetId()) for NI in self.graph.Nodes()]
+        return {"ClosenessCentr": CloseCentr}
+
+    def get_hubs_auth(self) -> dict:
+        """Calculates and returns the Hubs and Authority scores for all nodes"""
+        NIdHubH, NIdAuthH = self.graph.GetHits()
+
+        res = {**self.snap_to_array(NIdHubH, "HubScore"), **self.snap_to_array(NIdAuthH, "AuthScore")}
+        return res
+
+    def get_metrics(self) -> pd.DataFrame:
+        """Generates the following metrics: Betweenness, Closeness, PageRank, Authority and Hub Score"""
+        metrics: dict = {}
+        metrics.update(self.get_PageRank())
+        metrics.update(self.get_betweeness_centr())
+        metrics.update(self.get_closeness_centr())
+        metrics.update(self.get_hubs_auth())
+        return pd.DataFrame(metrics)
+
+class ResultPlot:
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def generate_plots(self):
+        """Calls required methods to generate plots"""
+        self.betweenness_closeness()
+        self.auth_hub()
+
+    def betweenness_closeness(self):
+        """Plots Betweenness, Closeness and PageRank"""
+
+        # Keep only columns needed
+        _df = self.df[["PageRank", "ClosenessCentr", "BetweennessCentr"]]
+        x_tick_labels = _df.index.values
+
+        # define axes and titles
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        ax1.set_xlabel("Node ID")
+        ax1.set_ylabel("PageRank")
+
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("ClosenessCentr")
+
+        ax3 = ax1.twinx()
+        ax3.set_ylabel("BetweennessCentr")
+
+
+        # plot the axes
+        _df.reset_index(inplace=True)
+        p1 = _df.PageRank.plot(kind="bar", color="tab:gray", ax=ax1, alpha=0.5, label="PageRank")
+        p2 = _df.ClosenessCentr.plot(kind="line", color="tab:orange", ax=ax3, label="ClosenessCentr")
+        p3 = _df.BetweennessCentr.plot(kind="line", color="tab:blue", ax=ax2, label="BetweennessCentr")
+
+        ax1.set_xticklabels(x_tick_labels)
+        ax3.spines['right'].set_position(('outward', 50))
+
+        # coloring
+        ax1.yaxis.label.set_color("tab:gray")
+        ax1.tick_params(axis='y', colors='tab:gray')
+        ax2.yaxis.label.set_color("tab:orange")
+        ax2.tick_params(axis='y', colors='tab:orange')
+        ax3.yaxis.label.set_color("tab:blue")
+        ax3.tick_params(axis='y', colors='tab:blue')
+
+        fig.tight_layout()
+        fig.savefig("plots/betweenness_closeness_pagerank.png")
+        plt.show()
+
+    def auth_hub(self):
+        """Plots Hub and Authority scores and PageRank"""
+
+        # Keep only columns needed
+        _df = self.df[["PageRank", "HubScore", "AuthScore"]]
+        x_tick_labels = _df.index.values
+
+        # define axes and titles
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        ax1.set_xlabel("Node ID")
+        ax1.set_ylabel("PageRank")
+
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("HubScore")
+
+        ax3 = ax1.twinx()
+        ax3.set_ylabel("AuthScore")
+
+
+        # plot the axes
+        _df.reset_index(inplace=True)
+        p1 = _df.PageRank.plot(kind="bar", color="tab:gray", ax=ax1, alpha=0.5, label="PageRank")
+        p2 = _df.HubScore.plot(kind="line", color="tab:orange", ax=ax3, label="HubScore")
+        p3 = _df.AuthScore.plot(kind="line", color="tab:blue", ax=ax2, label="AuthScore")
+
+        ax1.set_xticklabels(x_tick_labels)
+        ax3.spines['right'].set_position(('outward', 50))
+
+        # coloring
+        ax1.yaxis.label.set_color("tab:gray")
+        ax1.tick_params(axis='y', colors='tab:gray')
+        ax2.yaxis.label.set_color("tab:orange")
+        ax2.tick_params(axis='y', colors='tab:orange')
+        ax3.yaxis.label.set_color("tab:blue")
+        ax3.tick_params(axis='y', colors='tab:blue')
+
+        fig.tight_layout()
+        fig.savefig("plots/auth_hub_pagerank.png")
+        plt.show()
